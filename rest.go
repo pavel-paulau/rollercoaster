5c0d9eb81096e4d83cb18de367f4499827eba5f2
Pavel Paulau <pavel.paulau@gmail.com>
2016-09-06 13:56:02 -0700

    Reduce complexity of HTTP engine

--- rest.go
+++ rest.go
@@ -11,6 +11,60 @@ func init() {
 	gin.SetMode("release")
 }
 
+func getBenchmarks(c *gin.Context) {
+	var benchmarks []benchmark
+
+	kvPairs := make(chan kvPair, 10)
+
+	go iter(kvPairs)
+	for p := range kvPairs {
+		var b benchmark
+		if err := json.Unmarshal(p.value, &b); err != nil {
+			c.IndentedJSON(500, gin.H{"message": err.Error()})
+			return
+		}
+		b.ID = p.key
+		benchmarks = append(benchmarks, b)
+	}
+
+	c.IndentedJSON(200, benchmarks)
+}
+
+func addBenchmark(c *gin.Context) {
+	var b benchmark
+	if err := c.BindJSON(&b); err != nil {
+		c.IndentedJSON(400, gin.H{"message": err.Error()})
+		return
+	}
+	if b.Timestamp == 0 {
+		b.Timestamp = time.Now().UnixNano()
+	}
+
+	value, _ := json.Marshal(b) // Ignoring errors because they are not really possible
+
+	if err := put(b.ID, value); err != nil {
+		c.IndentedJSON(500, gin.H{"message": err.Error()})
+		return
+	}
+	c.IndentedJSON(201, gin.H{"message": "ok"})
+}
+
+func deleteBenchmark(c *gin.Context) {
+	var payload struct {
+		ID uint64 `json:"id"`
+	}
+	if err := c.BindJSON(&payload); err != nil {
+		c.IndentedJSON(400, gin.H{"message": err.Error()})
+		return
+	}
+
+	if err := del(payload.ID); err != nil {
+		c.IndentedJSON(500, gin.H{"message": err.Error()})
+		return
+	}
+	c.IndentedJSON(200, gin.H{"message": "ok"})
+}
+
 func httpEngine() *gin.Engine {
 	router := gin.Default()
 
@@ -18,60 +72,9 @@ func httpEngine() *gin.Engine {
 	router.Static("/static", "./static")
 
 	v1 := router.Group("/api/v1")
-
-	v1.GET("benchmarks", func(c *gin.Context) {
-		var benchmarks []benchmark
-
-		kvPairs := make(chan kvPair, 10)
-
-		go iter(kvPairs)
-		for p := range kvPairs {
-			var b benchmark
-			if err := json.Unmarshal(p.value, &b); err != nil {
-				c.IndentedJSON(500, gin.H{"message": err.Error()})
-				return
-			}
-			b.ID = p.key
-			benchmarks = append(benchmarks, b)
-		}
-
-		c.IndentedJSON(200, benchmarks)
-	})
-
-	v1.POST("benchmarks", func(c *gin.Context) {
-		var b benchmark
-		if err := c.BindJSON(&b); err != nil {
-			c.IndentedJSON(400, gin.H{"message": err.Error()})
-			return
-		}
-		if b.Timestamp == 0 {
-			b.Timestamp = time.Now().UnixNano()
-		}
-
-		value, _ := json.Marshal(b) // Ignoring errors because they are not really possible
-
-		if err := put(b.ID, value); err != nil {
-			c.IndentedJSON(500, gin.H{"message": err.Error()})
-			return
-		}
-		c.IndentedJSON(201, gin.H{"message": "ok"})
-	})
-
-	v1.DELETE("benchmarks", func(c *gin.Context) {
-		var payload struct {
-			ID uint64 `json:"id"`
-		}
-		if err := c.BindJSON(&payload); err != nil {
-			c.IndentedJSON(400, gin.H{"message": err.Error()})
-			return
-		}
-
-		if err := del(payload.ID); err != nil {
-			c.IndentedJSON(500, gin.H{"message": err.Error()})
-			return
-		}
-		c.IndentedJSON(200, gin.H{"message": "ok"})
-	})
+	v1.GET("benchmarks", getBenchmarks)
+	v1.POST("benchmarks", addBenchmark)
+	v1.DELETE("benchmarks", deleteBenchmark)
 
 	return router
 }
